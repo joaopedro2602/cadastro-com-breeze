f7e9329128c48d7fd9e0ecb680b76b5c2af6a888
nomemome <nomenome@gmail.com>
2023-07-28 16:33:13 -0300

    v1

---
+++ resources/views/cadastrar-cliente.blade.php
@@ -0,0 +1,70 @@
+<x-app-layout>
+    <x-slot name="header">
+        <h2 class="font-semibold text-xl text-gray-800 leading-tight">
+            {{ __('Cadastrar Cliente') }}
+        </h2>
+    </x-slot>
+    
+
+    <div class="py-12">
+        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+            <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
+                <div class="p-6 text-gray-900">
+                <form method="POST" action="{{ route('register') }}">
+        @csrf
+
+        <!-- Nome -->
+        <div>
+            <x-input-label for="name" :value="__('Nome')" />
+            <x-text-input id="name" class="block mt-1 w-full" type="text" name="name" :value="old('name')" required autofocus autocomplete="name" />
+            <x-input-error :messages="$errors->get('name')" class="mt-2" />
+        </div>
+
+        <!-- Endereço -->
+        <div class="mt-4">
+            <x-input-label for="email" :value="__('Endereço')" />
+            <x-text-input id="email" class="block mt-1 w-full" type="email" name="email" :value="old('email')" required autocomplete="username" />
+            <x-input-error :messages="$errors->get('email')" class="mt-2" />
+        </div>
+
+       <!-- Bairro -->
+       <div class="mt-4">
+            <x-input-label for="email" :value="__('Bairro')" />
+            <x-text-input id="email" class="block mt-1 w-full" type="email" name="email" :value="old('email')" required autocomplete="username" />
+            <x-input-error :messages="$errors->get('email')" class="mt-2" />
+        </div>
+
+        <!-- CEP -->
+       <div class="mt-4">
+            <x-input-label for="email" :value="__('CEP')" />
+            <x-text-input id="email" class="block mt-1 w-full" type="email" name="email" :value="old('email')" required autocomplete="username" />
+            <x-input-error :messages="$errors->get('email')" class="mt-2" />
+        </div>
+
+        <!-- Cidade -->
+       <div class="mt-4">
+            <x-input-label for="email" :value="__('Cidade')" />
+            <x-text-input id="email" class="block mt-1 w-full" type="email" name="email" :value="old('email')" required autocomplete="username" />
+            <x-input-error :messages="$errors->get('email')" class="mt-2" />
+        </div>
+
+        <!-- Estado -->
+       <div class="mt-4">
+            <x-input-label for="email" :value="__('Cidade')" />
+            <x-text-input id="email" class="block mt-1 w-full" type="email" name="email" :value="old('email')" required autocomplete="username" />
+            <x-input-error :messages="$errors->get('email')" class="mt-2" />
+        </div>
+
+        <div class="flex items-center justify-end mt-4">
+            
+
+            <x-primary-button class="ml-4">
+                {{ __('Registrar') }}
+            </x-primary-button>
+        </div>
+    </form>
+                </div>
+            </div>
+        </div>
+    </div>
+</x-app-layout>
